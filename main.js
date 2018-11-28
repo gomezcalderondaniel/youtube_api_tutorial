0c2c49263a799e7649254299474a9ea69280c474
gomezcalderondaniel <42280710+gomezcalderondaniel@users.noreply.github.com>
2018-11-27 19:14:47 -0700

    Update main.js

--- main.js
+++ main.js
@@ -80,7 +80,7 @@ function showChannelData(data) {
 
 // Get channel from API
 function getChannel(channel) {
-  gapi.client.youtube.searchz
+  gapi.client.youtube.search
     .list({
       part: 'snippet,contentDetails,statistics',
       forUsername: channel
